69e4a4c6a769a7e889bcb098446fbea04840a7ba
Anastasia <anast.korotkova2018@yandex.ru>
2022-09-01 23:21:40 +0300

    feat: add basic page layout

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 import config
 
 app = Flask(__name__)
 app.config.from_object(config.DevelopmentConfig)
 
+posts = [
+    {
+        'author': 'Anastasia',
+        'title': 'Пост №1', 
+        'content': 'Содержание поста №1',
+        'date_posted': 'Сентябрь 1, 2022'
+    }, 
+    {
+        'author': 'Maria',
+        'title': 'Пост №2', 
+        'content': 'Содержание поста №2',
+        'date_posted': 'Сентябрь 2, 2022'
+    }
+]
+
 
 @app.route("/")
-def home():
-    return "Hello, Flask!"
+@app.route('/index')
+def index():
+    return render_template('index.html', posts=posts, title='Главная')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='О блогe')
