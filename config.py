e6d0c64c8d68ad3fa68a96049786f652b5ca1bbf
Anastasia <anast.korotkova2018@yandex.ru>
2022-09-02 14:59:06 +0300

    refactor: change config

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
 
-class Config:
-    SECRET_KEY = environ.get('SECRET_KEY')
+class Config(object):
+    SECRET_KEY = environ.get('SECRET_KEY') or "\x9fE\xd8'f\xc2h\xa9\x91\xb9\xa0DpE\xa54\xa0\x8a\x95^\xa4\xdf\xf7\xbd"
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
 
