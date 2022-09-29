78a9a7db753829c512dce87a299ad2ceb0149a9c
Anastasia <anast.korotkova2018@yandex.ru>
2022-09-29 21:45:25 +0300

    refactor: change configuration

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-import config
 
 
 app = Flask(__name__)
-app.config.from_object(config.DevelopmentConfig)
+app.config.from_object('config.DevelopmentConfig')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
--- config.py
+++ config.py
@@ -5,11 +5,10 @@ basedir = path.abspath(path.dirname(__file__))
 load_dotenv(path.join(basedir, '.env'))
 
 
-class Config(object):
+class Config:
     SECRET_KEY = environ.get('SECRET_KEY')
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'  # environ.get('DATABASE_URL', 'sqlite:///site.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
@@ -17,9 +16,11 @@ class DevelopmentConfig(Config):
     FLASK_ENV = 'development'
     DEBUG = True
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = environ.get('DEV_DATABASE_URI')
 
 
 class ProductionConfig(Config):
     FLASK_ENV = 'production'
     DEBUG = False
     TESTING = False
+    SQLALCHEMY_DATABASE_URI = environ.get('PROD_DATABASE_URI')
