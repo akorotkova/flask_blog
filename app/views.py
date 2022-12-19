3d3bdfbef3f4cc6782b0b46859027131926cf5c9
Anastasia <anast.korotkova2018@yandex.ru>
2022-12-19 17:40:23 +0300

    feat: add pagination

--- app/views.py
+++ app/views.py
@@ -11,7 +11,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/")
 @app.route('/index')
 def index():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.paginate(page=page, per_page=5)
     return render_template('index.html', posts=posts, title='Главная')
 
 
