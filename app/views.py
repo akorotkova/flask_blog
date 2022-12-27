13235da682b629e870fea9ba95dfbc8ac110368f
Anastasia <anast.korotkova2018@yandex.ru>
2022-12-27 18:04:05 +0300

    refactor: change application configuration (add email configurations)

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from . import app, db, bcrypt
+from . import app, db, bcrypt, mail
 from .forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm, RequestResetForm, ResetPasswordForm
 from .models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
