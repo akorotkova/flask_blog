757f0ebb53e9ec2e12bd0ceb564129174d942faa
Anastasia <anast.korotkova2018@yandex.ru>
2023-01-21 20:27:14 +0300

    feat: add logging (write to file)

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
+import os
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -42,5 +43,18 @@ def create_app(config_class=Config):
                 credentials=(app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD']), secure=())
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+        
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+
+        file_handler = RotatingFileHandler('logs/flask_blog.log', maxBytes=10240, backupCount=10, encoding='utf-8')
+        strfmt = '[%(asctime)s] [%(levelname)s]: [%(message)s] [in %(pathname)s:%(lineno)d]'
+        datefmt = '%d-%m-%Y %H:%M:%S'
+        file_handler.setFormatter(logging.Formatter(fmt=strfmt, datefmt=datefmt))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('flask_blog startup')
 
     return app
