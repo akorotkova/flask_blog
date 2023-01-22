8bddc24672c13565d17283e45f13ea7bb64032d9
Anastasia <anast.korotkova2018@yandex.ru>
2023-01-22 12:26:02 +0300

    feat: add 405 error handler

--- app/errors/views.py
+++ app/errors/views.py
@@ -2,15 +2,17 @@ from . import errors
 from flask import render_template
 
 
-@errors.app_errorhandler(404)
-def error_404(error):
-    return render_template('errors/404.html'), 404
-
-
 @errors.app_errorhandler(403)
 def error_403(error):
     return render_template('errors/403.html'), 403
 
+@errors.app_errorhandler(404)
+def error_404(error):
+    return render_template('errors/404.html'), 404
+
+@errors.app_errorhandler(405)
+def error_405(error):
+    return render_template('errors/405.html'), 405
 
 @errors.app_errorhandler(500)
 def error_500(error):
